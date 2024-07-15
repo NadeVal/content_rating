2762c6bd054d15feccc30ea335730c6cdde60d9d
Nada Velkova <mail2nadica@yahoo.com>
2024-07-15 15:59:45 +0200

    changing CongentRating.jsx again

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,25 +7,38 @@ class ContentRating extends Component {
     super();
     this.state = {
       likes: 0,
-      dislikes: 0
-    };
+      dislikes: 0,
+      totalRatings: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+  }
   }
   render() {
     return (
      <>
-     {/* <h1>Text Content Rating</h1> */}
-     <div className='content-rating'>
+        <div className='content-rating'>
         <p>
         ---Add text here---
         </p>
         <div className='rating-buttons'>
-        <button className="like-button">
+        <button className="like-button" onClick={this.state.handleLike}>
             Like ({this.state.likes})
           </button>
-          <button className="dislike-button">
+          <button className="dislike-button" onClick={this.state.handleDislike}>
             Dislike ({this.state.dislikes})
-          </button>
+</button>
         </div>
+        <p>Total Ratings: {this.state.totalRatings}</p>
     </div>
      </>
     );
